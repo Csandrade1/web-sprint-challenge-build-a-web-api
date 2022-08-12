1d98633b9129813b47aa15141a16f0d9977d650f
Csandrade1 <andrade.cynthia07@gmail.com>
2022-08-12 14:19:19 -0700

    first commit

--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
-const express = require('express');
+const express = require("express");
+const ProjectsRouter = require("./projects/projects-router");
+const ActionsRouter = require("./actions/actions-router");
 const server = express();
 
+server.use(express.json());
+
+server.use("/api/projects", ProjectsRouter);
+
+server.use("/api/actions", ActionsRouter);
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
